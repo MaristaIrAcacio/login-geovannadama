b83e00a6e8230f643520cef67b5830b9ef40bf03
Geovanna Dama <geovannadama9987@gmail.com>
2023-07-26 12:57:53 -0300

    atualização do js

--- controller.js
+++ controller.js
@@ -8,3 +8,21 @@ function acessar() {
       window.location.href = "aula2.html";
     }
 }
+
+var dadosLista = [];
+
+function salvarUser(){
+  let nomeUser = document.getElementById("nomeUser").value;
+
+  if(nomeUser){
+    dadosLista.push(nomeUser);
+  
+  }
+}
+function criarLista(){
+  let tabela = document.getElementById(' tabela ').innerHTML ="<tr> <th>Nome Usuário</th> <th>Ações</th></tr>";
+
+  for( let i = 0;i <= (dadosLista.length - 1);i++){
+    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button>Editar</button"
+  }
+}
