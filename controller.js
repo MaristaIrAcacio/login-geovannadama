c1a8cd2701a5c8de9fb95e9ca507261faaa503a5
Geovanna Dama <geovannadama9987@gmail.com>
2023-07-31 11:40:14 -0300

    criação da lista de usuario, alteração do js

--- controller.js
+++ controller.js
@@ -1,28 +1,74 @@
 function acessar() {
-    let loginEmail = document.getElementById("loginEmail").value;
-    let loginSenha = document.getElementById("loginSenha").value;
- 
-    if(!loginEmail || !loginSenha) {
-        alert("favor preencha os campos para acessar o site");
-    } else {
-      window.location.href = "aula2.html";
-    }
+
+  let loginEmail = document.getElementById("loginEmail").value;
+
+  let loginSenha = document.getElementById("loginSenha").value;
+
+
+
+  if(!loginEmail || !loginSenha) {
+
+      alert("favor preencha os campos para acessar o site");
+
+  } else {
+
+     window.location.href = "aula2.html";
+
+  }
+
 }
+
+
+
+
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+
+
+
 
 var dadosLista = [];
 
+
+
+
 function salvarUser(){
-  let nomeUser = document.getElementById("nomeUser").value;
+
+  let nomeUser=document.getElementById("nomeUser").value;
+
+
+
 
   if(nomeUser){
-    dadosLista.push(nomeUser);
-  
-  }
-}
-function criarLista(){
-  let tabela = document.getElementById(' tabela ').innerHTML ="<tr> <th>Nome Usuário</th> <th>Ações</th></tr>";
 
-  for( let i = 0;i <= (dadosLista.length - 1);i++){
-    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button>Editar</button"
+      dadosLista.push(nomeUser);
+
+      criarLista();
+
   }
+
+}
+
+
+
+
+// FUNÇÃO PARA CRIAR LISTA DE USUÁRIO
+
+
+
+
+function criarLista(){
+
+let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+for(let i = 0;i <= (dadosLista.length - 1);i++){
+
+  tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger'  onclick=''>Excluir</button></td></tr>";
+
+  document.getElementById('tabela').innerHTML = tabela;
+
+
+
+
+  }
+
 }
